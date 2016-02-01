5259d1edfc7cb6e2719b7e0946f1009eb19a954e
root <isanfer91@gmail.com>
2016-02-01 19:02:56 +0100

    cambiado php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 		$dbname = getenv("OPENSHIFT_APP_NAME");
 
 		mysql_connect($dbhost, $dbuser, $dbpassword) or die(mysql_error());
-		mysql_set_charset("utf8")
+		mysql_set_charset("utf8");
 mysql_select_db($dbname) or die(mysql_error());
 		$result = mysql_query("SELECT * FROM Usuarios") or die(mysql_error());
 	?>
